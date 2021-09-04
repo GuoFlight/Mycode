e0428649997329277e2430442ff9d64c2a384dfe
GuoFlight <1754811791@qq.com>
2021-09-04 22:29:10 +0800

    新增golang的tools函数

--- tools/tools.go
+++ tools/tools.go
@@ -76,3 +76,78 @@ func GetUGid(user string)(uid,gid int,err error){
 	return uid,gid,nil
 }
 
+// 获取第一个非 loopback ip
+func LocalIP() (net.IP, error) {
+	tables, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, t := range tables {
+		addrs, err := t.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, a := range addrs {
+			ipnet, ok := a.(*net.IPNet)
+			if !ok || ipnet.IP.IsLoopback() {
+				continue
+			}
+			if v4 := ipnet.IP.To4(); v4 != nil {
+				return v4, nil
+			}
+		}
+	}
+	return nil, fmt.Errorf("cannot find local IP address")
+}
+
+//将域名转化为ip
+func ServernameToIP(servernames []string) (map[string]string, error) {
+	ret := make(map[string]string)
+	for _,hostname := range servernames{
+		cmd := exec.Command("host", hostname)
+		output, err :=cmd.Output()			//执行程序，返回标准输出[]byte
+		if err!=nil{
+			return nil,errors.New(fmt.Sprintf("获取%s ip失败,请检查域名列表", hostname))
+		}
+
+		r := regexp.MustCompile("[\\d]+.[\\d]+.[\\d]+.[\\d]+")
+		RetRegexp := r.FindString(string(output))
+		if RetRegexp==""{
+			return nil,errors.New(fmt.Sprintf("获取%s ip失败,请检查域名列表", hostname))
+		}
+		ret[hostname] = RetRegexp
+	}
+	return ret, nil
+}
+
+//查看某用户是否拥有免密切换到root的权限
+func HasCurHostRootPermissionNoPasswd(user string)(ret bool){
+	var cmd *exec.Cmd
+	cmd = exec.Command("/bin/bash","-c","sudo -iu "+user+" sudo -i pwd")
+	_,err := cmd.Output()
+	if err!=nil{
+		return false
+	}
+	return true
+}
+
+//获取当前用户
+func GetCurUser()(string,error){
+	cmd := exec.Command("whoami")
+	output, err :=cmd.Output()			//执行程序，返回标准输出[]byte
+	if err!=nil{
+		return "", err
+	}
+	return strings.TrimSpace(string(output)),nil
+}
+
+
+//函数作用：String类型的"\u91d1\u989d"转换成Unicode的"金额"
+func StringToUnicode(strOrigin string) (string, error) {
+	str, err := strconv.Unquote(strings.Replace(strconv.Quote(strOrigin), `\\u`, `\u`, -1))
+	if err != nil {
+		return "", err
+	}
+	return str, nil
+}
+
