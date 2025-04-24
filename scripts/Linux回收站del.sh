8e26e870c562caa2d7ccec49b9ab2139b37858fa
GuoFlight <1754811791@qq.com>
2025-04-25 01:17:29 +0800

    feat del回收站支持删除多文件

--- scripts/Linux回收站del.sh
+++ scripts/Linux回收站del.sh
@@ -2,44 +2,43 @@
 
 #################################################################
 # 脚本作用: Linux的回收站
-# 作者: 郭少
-# 版本: v2.0
-# 最近一次更新时间:2023-08-19
+# 作者: 京城郭少
+# 版本: v3.0
+# 最近一次更新时间: 2025-04-24
 #################################################################
-
-garbage_dir="$HOME/garbage"
-target="$1"
 set -e
+garbage_dir="$HOME/.garbage"
 
 function init(){
-        #若文件不存在
-        if [ ! -e "$target" ]; then
-                echo "删除失败 '$target': No such file or directory"
-                exit 1
-        fi
-        #创建垃圾桶
+        # 创建回收站
         mkdir -p $garbage_dir
 }
 
-get_new_filename(){
-    #文件名与删除时间的分隔符
+function get_new_filename(){
+    # 文件名与删除时间的分隔符
     sep_char="_"
-    
-    #分离出文件名
+
+    # 分离出文件名
     old_filename=`basename $target`
 
-    #得到新文件名
-    cur_time=`date "+%Y%m%d%H%M%S"`
+    # 得到新文件名
+    cur_time=`date "+%Y%m%d%H%M%S${sep_char}${RANDOM}"`
     new_filename="$old_filename$sep_char$cur_time"
-        
-    echo $new_filename  # 返回值
+
+    # 返回结果       
+    echo $new_filename
     return 0
+}
+function del_target(){
+    target=$1
+    [ "$target" == "" ] && echo "程序异常" && exit 1
+    new_filename=`get_new_filename`
+    mv $target $garbage_dir/$new_filename
 }
 
 ################################## mian ##################################
 
 init
-
-new_filename=`get_new_filename`
-
-mv $target $garbage_dir/$new_filename
+for target in "$@"; do 
+    del_target $target
+done
